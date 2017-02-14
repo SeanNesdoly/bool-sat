d6abe9b868b73ff536e8a4a72672ab5c271dbf2a
srnesdoly <srnesdoly@me.com>
2017-02-14 15:48:44 -0500

    created data structures for literals, clauses, and CNF formulae; implemented find_unit_clause & unit_propogate helper methods for DPLL

--- DPLL.java
+++ DPLL.java
@@ -8,63 +8,193 @@
  */
 
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
 import java.io.IOException;
 
 public class DPLL {
 
-    private static final String SATISFIABLE = "The conclusion follows logically from the premises.\n";
-    private static final String NOT_SATISFIABLE = "The conclusion does not follow logically from the premises.\n";
+    private static final String SAT = "The conclusion follows logically from the premises.\n";
+    private static final String UNSAT = "The conclusion does not follow logically from the premises.\n";
+
+    public boolean dpll(Set<Literal> literals, Set<Clause> clauses) {
+
+
+        return false;
+    }
+
+    // find the first instance of a unit clause in the formula; note that a unit clause
+    // is simply a clause that contains only one literal
+    private Clause find_unit_clause(CNF formula) {
+        for(Iterator<Clause> i = formula.clauses.iterator(); i.hasNext();) {
+            Clause c = i.next();
+
+            if (c.isUnitClause())
+                return c;
+        }
+
+        return null;
+    }
+
+    // look for all non-unit clauses that contain the unit clause l and remove them,
+    // as there exists an assignment of the unit clause that makes all clauses with that literal
+    // true; additionally, remove from all clauses any instance of the negated unit clause
+    private CNF unit_propogate(Literal l, CNF formula) {
+        Literal negated_l = l.createNegatedLiteral();
+
+        for(Iterator<Clause> i = formula.clauses.iterator(); i.hasNext();) {
+            Clause c = i.next();
+
+            // remove all non-unit clauses that contain the unit clause l
+            if (!c.isUnitClause() && c.literals.indexOf(l) != -1) {
+                i.remove();
+                continue;
+            }
+
+            // remove all instances of the negated unit clause for each clause in the formula
+            while (c.literals.indexOf(negated_l) != -1) {
+                // we are not modifying the actual iterator here so it is a safe operation!
+                c.literals.remove(negated_l);
+            }
+        }
+
+        return formula;
+    }
+
+    // TODO: has been replaced by unit_propogate.. will remove
+    private void propogate_units(CNF formula) {
+        for(Iterator<Clause> iterator1 = formula.clauses.iterator(); iterator1.hasNext();) {
+            Clause c = iterator1.next();
+
+            if (c.isUnitClause()) {
+                //Literal l = c.literals.toArray(new Literal[1])[0]; // grab the only literal in the clause
+
+                // look for all non-unit clauses that contain the single literal x in c and remove them,
+                // as there exists an assignment of the unit clause c that makes all clauses with x true
+                for(Iterator<Clause> iterator2 = formula.clauses.iterator(); iterator2.hasNext();) {
+                    Clause other = iterator2.next();
+
+                    // remove all non-unit clauses that contain the literal unit cluase c
+                    /*if (!other.isUnitClause() && other.contains(c)) {
+                        iterator2.remove(); //formula.clauses.remove(other);
+                    }*/
+
+                    // remove all instances of the negated literal c in every clause
+
+                }
+            }
+        }
+    }
+
+    private void pure_literal_elimination(CNF formula) {
+
+    }
+
+
+
+    // Wrapper class for a propositional logic literal (A | !A).
+    // A positive literal is represented internally as a positive int (+x); a
+    // negavtive literal is represented internally as a negative int (-x)
+    private class Literal {
+        char s; // symbol of literal
+        int l; // +l = a positive literal; -l = a negative literal
+        boolean val; // the value assigned to this literal
+        boolean isAssigned; // flag for this literal being assigned in the DPLL algorithm
+
+        public Literal(char symbol, boolean sign) {
+            s = symbol;
+            if (sign)
+                l = (int)symbol; // positive literal
+            else
+                l = -symbol; // negative literal
+
+            isAssigned = false; // maybe not?
+        }
+
+        // Parse out a Literal from a String!
+        public Literal(String litStr) {
+            if (litStr.length() == 0)
+                System.err.println("Invalid literal instantiation.");
+            else if (litStr.length() == 1) { // positive literal A
+                s = litStr.charAt(0);
+                l = (int)s;
+            } else { // negative literal !A
+                s = litStr.charAt(1);
+                l = -s;
+            }
+
+            isAssigned = false;
+        }
+
+        // compute the value of the literal with consideration of its sign
+        public boolean computeValue() {
+            if (l>0) // positive literal
+                return val;
+            else // negative literal
+                return !val;
+        }
+
+        // negate the literal by flipping the sign of the integer representation
+        public void negate() {
+            l = -l;
+        }
+
+        public Literal createNegatedLiteral() {
+            return new Literal(this.s, this.l > 0 ? false:true);
+        }
+    }
+
+
+
+    // Wrapper class for the disjunction of a set of Literals
+    private class Clause {
+
+        LinkedList<Literal> literals;
+
+        public Clause(String clauseStr) {
+            // TODO: parse literals from clause string
+        }
+
+        public Clause(LinkedList<Literal> _literals) {
+            literals = _literals;
+        }
+
+        public Clause(Literal unitClause) {
+            literals = new LinkedList<Literal>();
+            literals.add(unitClause);
+        }
+
+        // determines if this clause is a unit clause (only 1 literal)
+        public boolean isUnitClause() {
+            return literals.size() == 1;
+        }
+    }
+
+    // Wrapper class for a propositional logic formula in CNF
+    private class CNF {
+        Set<Literal> literals;
+        Set<Clause> clauses;
+        String cnf; // CNF formula in clausal form
+
+        public CNF(String _cnf) {
+            cnf = _cnf;
+            // TODO: parse CNF string into Clauses & Literals
+        }
+    }
 
     public static void main(String[] args) {
 
-        ArrayList<String> cnf_string_formula = null;
+        ArrayList<String> formulas = null;
         try {
-            cnf_string_formula = TextFile.readFile();
-            TextFile.writeFile(NOT_SATISFIABLE);
+            formulas = TextFile.readFile();
+            //System.out.println(formulas.get(0));
+            TextFile.writeFile(SAT);
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
             return;
         }
 
-        //parseCNF(cnf_string_formula);
-    }
-
-    public static char[][] parseCNF(String cnf_string_formula) {
-        // convert to all uppercase & trim off leading & ending braces
-        cnf_string_formula = cnf_string_formula.toUpperCase().substring(1,cnf_string_formula.length()-1);
-
-        //String delim = "(\\)\\W\\()";
-        String delim = ",";
-        String[] clauses = cnf_string_formula.split(delim);
-
-        char[][] cnf_formula = new char[clauses.length][];
-
-        for (int i = 0; i < clauses.length; i++) {
-            System.out.println(clauses[i]);
-            String[] literals = clauses[i].split(",");
-            cnf_formula[i] = new char[literals.length];
-
-            for (int j = 0; j < literals.length; j++) {
-                if (literals[j].length() > 1) {
-                    // case: false literal !A --> convert to LOWERCASE
-                    //cnf_formula[i][j] = literals[j].toLowerCase().charAt(1);
-                } else {
-                    // case: true literal A --> keep UPPERCASE
-                    //cnf_formula[i][j] = literals[j].charAt(0);
-                }
-            }
-        }
-
-        return cnf_formula;
-    }
-
-    private class Symbol {
-        public char _s;
-        public boolean _value;
-
-        Symbol(char s, boolean value) {
-            _s = s;
-            _value = value;
-        }
     }
  }
