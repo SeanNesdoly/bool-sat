6659adb334943e01f5ea6e820e14220de3edade1
Mary Hoekstra <maryhoekstra@Marys-MacBook-Pro.local>
2017-02-17 06:51:44 -0500

    Skeleton file for ThreeColour problem

---
+++ ThreeColour.java
@@ -0,0 +1,17 @@
+/*
+ * Given an set of edges in a graph, ThreeColour solves the Three-Colouring Problem,
+ * which is described as follows: Given a map, is it possible to colour the countries
+ * using three colours such that no two adjacent countries have the same colour?
+ * If the problem can be solved, the output is a list of the vertices with their 
+ * respective colours.
+ *
+ * CISC 352 Assignment 2
+ * Sean Nesdoly & Mary Hoekstra
+ * February 16th, 2017
+*/
+
+//package converttocnf;
+
+public class ThreeColour {
+    
+}
