5b354f0906714932dea0f1a6a07d862b32f3c9d3
Mary Hoekstra <maryhoekstra@Marys-MacBook-Pro.local>
2017-03-03 10:13:19 -0500

    started parsing for ThreeColour - creates vertex set and creates 4 clauses for each vertex

--- src/boolsat/ThreeColour.java
+++ src/boolsat/ThreeColour.java
@@ -9,9 +9,69 @@
  * Sean Nesdoly & Mary Hoekstra
  * February 16th, 2017
 */
+//package threecolour;
 
-//package converttocnf;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class ThreeColour {
+    
+    
+    /* Given a string of input, extracts unique vertices and adds to a set structure. */
+    public static Set<String> createVertexSet(String input) {
+        Set<String> vertices = new LinkedHashSet<>();
+        input = input.substring(1,input.length()-1); // remove braces
+        input = input.replace("(", "");
+        input = input.replace(")","");
+        input = input.replace(",","");
+        input = input.replaceAll("\\s+",""); // strip whitespace
+        char vertex;
+        for (int i = 0; i < input.length(); i++) {
+            vertex = input.charAt(i);
+            vertices.add(Character.toString(vertex));    
+        }  
+        return vertices;
+    }
+    
+    /* Given a string of input, pairs vertices to create list of edges. */
+    public static ArrayList<String> createEdgeList(String input) {
+        ArrayList<String> edgeList = new ArrayList<>();
+        input = input.substring(1,input.length()-1); // remove braces
+        input = input.replace(",","");
+        // not finished
+        
+    }
+    
+    /* For each vertex, creates 4 clauses imposing colouring constraints. */
+    public static ArrayList<String> createVertexClauses(Set<String> vertices) {
+        ArrayList<String> clauseList = new ArrayList<>();
+        for (String vertex : vertices) {
+            clauseList.add(vertex + "Rv" + vertex + "Gv" + vertex + "B");
+            clauseList.add("!" + vertex + "Rv!" + vertex + "G");
+            clauseList.add("!" + vertex + "Gv!" + vertex + "B");
+            clauseList.add("!" + vertex + "Bv!" + vertex + "R");
+        }
+        return clauseList;
+    }
+    
+        /* For each vertex, creates 4 clauses imposing colouring constraints. */
+    public static ArrayList<String> createEdgeClauses(Set<String> edgeList) {
+        ArrayList<String> clauseList = new ArrayList<>();
+        for (String edge : edgeList) {
+
+        }
+        return clauseList;
+    }
+    
+    public static void main(String[] args) {
+        String input = "{(A,B),(A,C),(A,D),(B,C),(B,D),(C,D)}";
+        Set<String> vertices = createVertexSet(input);
+        ArrayList<String> edges = createEdgeList(input);
+        ArrayList<String> clauseList = createVertexClauses(vertices);
+
+    }
+    
     
 }
